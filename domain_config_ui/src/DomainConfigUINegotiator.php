10e85552ede172bb3d279f6c1f730319a551e60c
Andy Postnikov <apostnikov@gmail.com>
2017-03-12 22:47:20 +0200

    WIP domain detection for UI

--- domain_config_ui/src/DomainConfigUINegotiator.php
+++ domain_config_ui/src/DomainConfigUINegotiator.php
@@ -8,8 +8,34 @@ use Drupal\domain\DomainNegotiator;
  * {@inheritdoc}
  */
 class DomainConfigUINegotiator extends DomainNegotiator {
+
+  /**
+   * Semaphore to prevent negotiation infinite loop.
+   *
+   * @var bool
+   */
+  protected $negitiating = FALSE;
+
+  /**
+   * Set the current selected domain ID into session.
+   *
+   * @param string $domain_id
+   *   The domain ID to store in session.
+   */
+  public function setSelectedDomain($domain_id) {
+    if ($this->domainLoader->load($domain_id)) {
+      $_SESSION['domain_config_ui']['config_save_domain'] = $domain_id;
+    }
+    else {
+      $_SESSION['domain_config_ui']['config_save_domain'] = '';
+    }
+  }
+
   /**
    * Determine the active domain.
+   *
+   * @return \Drupal\domain\DomainInterface
+   *   The domain entity.
    */
   protected function negotiateActiveDomain() {
     // Set http host to be that of the selected domain to configure.
@@ -35,23 +61,36 @@ class DomainConfigUINegotiator extends DomainNegotiator {
 
   /**
    * Get the selected domain ID.
+   *
+   * @return string
+   *   The selected domain ID from session or current domain.
    */
   public function getSelectedDomainId() {
-    // Return selected domain ID on admin paths only.
-    return !empty($_SESSION['domain_config_ui']['config_save_domain']) ? $_SESSION['domain_config_ui']['config_save_domain'] : '';
+    if ($this->negitiating) {
+      // Current domain is unknown.
+      return '';
+    }
+    if (!$this->domain) {
+      $this->negitiating = TRUE;
+      // Initialize current domain.
+      parent::negotiateActiveDomain();
+      $this->negitiating = FALSE;
+    }
+    $route = \Drupal::routeMatch()->getRouteObject();
+    $isAdminRoute = \Drupal::service('router.admin_context')
+      ->isAdminRoute($route);
+    if (!$isAdminRoute) {
+      // Return selected domain ID on admin paths only.
+      return $this->getActiveId();
+    }
+    if (isset($_SESSION['domain_config_ui']['config_save_domain'])) {
+      if (!empty($_SESSION['domain_config_ui']['config_save_domain'])) {
+        // Override current domain with session stored one.
+        $this->domain = $this->domainLoader->load($_SESSION['domain_config_ui']['config_save_domain']);
+      }
+      return $_SESSION['domain_config_ui']['config_save_domain'];
+    }
+    return $this->getActiveId();
   }
 
-  /**
-   * Set the current selected domain ID.
-   *
-   * @param string $domain_id
-   */
-  public function setSelectedDomain($domain_id) {
-    if ($domain = $this->domainLoader->load($domain_id)) {
-      $_SESSION['domain_config_ui']['config_save_domain'] = $domain_id;
-    }
-    else {
-      $_SESSION['domain_config_ui']['config_save_domain'] = '';
-    }
-  }
 }
