34289d860aeb680df508335e0722f78cfa695079
Vasiliy Anosov <basil.snowman@gmail.com>
2017-04-24 17:40:26 +0300

    Added refreshing the page after changing the domain.

--- domain_config_ui/src/Form/SwitchForm.php
+++ domain_config_ui/src/Form/SwitchForm.php
@@ -1,11 +1,15 @@
 <?php
 namespace Drupal\domain_config_ui\Form;
 
+use Drupal\Core\Ajax\RedirectCommand;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\domain\DomainLoaderInterface;
 use Drupal\domain\DomainNegotiatorInterface;
 use Drupal\Core\Ajax\AjaxResponse;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class SwitchForm extends FormBase {
   /**
@@ -16,11 +20,24 @@ class SwitchForm extends FormBase {
   protected $domainNegotiator;
 
   /**
-   * Class constructor.
+   * The domain loader.
+   *
+   * @var \Drupal\domain\DomainLoaderInterface
    */
-  public function __construct(DomainNegotiatorInterface $domain_negotiator) {
+  protected $domainLoader;
+
+  /**
+   * Class constructor.
+   *
+   * @param DomainNegotiatorInterface $domain_negotiator
+   *   The Domain negotiator.
+   * @param DomainLoaderInterface $domain_loader
+   *   The domain loader.
+   */
+  public function __construct(DomainNegotiatorInterface $domain_negotiator, DomainLoaderInterface $domain_loader) {
     // Set the Domain negotiator.
     $this->domainNegotiator = $domain_negotiator;
+    $this->domainLoader = $domain_loader;
   }
 
   /**
@@ -28,7 +45,10 @@ class SwitchForm extends FormBase {
    */
   public static function create(ContainerInterface $container) {
     // Instantiates this form class with negotiator.
-    return new static($container->get('domain.negotiator'));
+    return new static(
+      $container->get('domain.negotiator'),
+      $container->get('domain.loader')
+    );
   }
 
   /**
@@ -41,9 +61,18 @@ class SwitchForm extends FormBase {
   /**
    * {@inheritDoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state) {
+  public function buildForm(array $form, FormStateInterface $form_state, Request $request = NULL) {
     // Only allow access to domain administrators.
     $form['#access'] = $this->currentUser()->hasPermission('administer domains');
+
+    // Fill current request path.
+    if ($request) {
+      $path = $request->getPathInfo();
+      $form['current_path'] = [
+        '#type' => 'value',
+        '#value' => $path,
+      ];
+    }
 
     // Add domain switch select field.
     if ($selected_domain = $this->domainNegotiator->getSelectedDomain()) {
@@ -55,7 +84,7 @@ class SwitchForm extends FormBase {
     $form['config_save_domain'] = array(
       '#type' => 'select',
       '#title' => 'Save config for:',
-      '#options' => array_merge(['' => 'All Domains'], \Drupal::service('domain.loader')->loadOptionsList()),
+      '#options' => array_merge(['' => 'All Domains'], $this->domainLoader->loadOptionsList()),
       '#default_value' => $selected,
       '#ajax' => array(
         'callback' => '::switchCallback',
@@ -79,11 +108,25 @@ class SwitchForm extends FormBase {
    * Callback to remember save mode.
    *
    * @param array $form
+   *   The form array.
    * @param FormStateInterface $form_state
+   *   The form state array.
+   *
+   * @return AjaxResponse
+   *   The response.
    */
   public function switchCallback(array &$form, FormStateInterface $form_state) {
     $this->domainNegotiator->setSelectedDomain($form_state->getValue('config_save_domain'));
     $response = new AjaxResponse();
+    // Refresh the page after changing the domain.
+    $path = $form['current_path']['#value'];
+    if ($path != '') {
+      $url = Url::fromUri('base:' . $path);
+      $url->setAbsolute();
+      $response->addCommand(new RedirectCommand($url->toString()));
+    }
     return $response;
   }
+
 }
+
